7b3b32d304ca1821e7c483dac4d7924e65cd0e4b
Jacob Francke <j.francke@findata.nl>
2022-07-15 13:23:17 +0200

    Updated fork to include the binary for Aarch64 systems

--- WKHTMLToPDF.php
+++ WKHTMLToPDF.php
@@ -1,7 +1,9 @@
 <?php
-namespace h4cc\WKHTMLToPDF;
+declare(strict_types=1);
+
+namespace blaater6\WKHTMLToPDF;
 
 class WKHTMLToPDF
 {
-    const PATH = __DIR__ . '/bin/wkhtmltopdf-amd64';
+    const PATH = __DIR__ . '/bin/wkhtmltopdf-aarch64';
 }
